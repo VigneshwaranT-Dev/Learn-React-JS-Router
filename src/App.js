e6585f6a8d5807d388352996cfdc550eb80ff0da
Vigneshwaran T <vigneshwarant54@gmail.com>
2024-07-18 22:42:19 +0530

    Very First Basic Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ul>
+          <li><Link to='home'>Home</Link></li>
+          <li><Link to='about'>About</Link></li>
+          <li><Link to='contact'>Contact</Link></li>
+        </ul>
+        <Routes>
+          <Route path='' element={<Navigate to="home" />} />
+          <Route path='home' element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='contact' element={<Contact/>} />
+          <Route path='*' element={<h1>404 Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
